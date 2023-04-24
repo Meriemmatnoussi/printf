c9301ca298fef1baf02800388af686a177d780e7
Meriemmatnoussi <meriem.matnoussi123@gmail.com>
2023-04-24 15:28:28 -0700

    0. I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life

--- _printf.c
+++ _printf.c
@@ -16,7 +16,9 @@ int _printf(const char *format, ...)
 va_list args;
 int count = 0;
 const char *p = format;
-	if (format == NULL)
+	if (!format || (format[0] == '%' && !format[1]))
+	return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
 	return (-1);
 	va_start(args, format);
 	while (*p != '\0')
