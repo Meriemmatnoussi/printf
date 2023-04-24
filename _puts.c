e2b76b048cef0818f3125bf166b77b0c9521e612
ElRoccinante <alaechouiye@gmail.com>
2023-04-24 08:16:05 -0700

    100-main_opcodes.c

--- _puts.c
+++ _puts.c
@@ -9,11 +9,11 @@
   *_putchar - writes a character to stdou
   *@c: character to print
   *
-  *Return: on succes 1. On error, -1 is returned, and errno is set appropriately.
+  *Return: on succes 1.On error,-1 is returned,and errno is set appropriately.
   */
 int _putchar(char c)
 {
-        return (write(1, &c, 1));
+	return (write(1, &c, 1));
 }
 #include <stdio.h>
 #include <stdarg.h>
@@ -30,16 +30,16 @@ int _putchar(char c)
   */
 int _puts(char *str)
 {
-        int count = 0;
+	int count = 0;
 
-        if (str == NULL)
-                str = "(null)";
+	if (str == NULL)
+		str = "(null)";
 
-        while (*str != '\0')
-        {
-                count += _putchar(*str);
-                str++;
-        }
+	while (*str != '\0')
+{
+		count += _putchar(*str);
+		str++;
+}
 
-        return (count);
+	return (count);
 }
