3b1641121d84819d5308de84b33ec670749f0c0e
Meriemmatnoussi <meriem.matnoussi123@gmail.com>
2023-04-24 18:41:05 -0700

    1. Education is when you read the fine print. Experience is what you get if you don't

--- _printf.c
+++ _printf.c
@@ -13,44 +13,70 @@
  */
 int _printf(const char *format, ...)
 {
-va_list args;
-int count = 0;
-const char *p = format;
+	va_list args;
+	int count = 0;
+
 	if (!format || (format[0] == '%' && !format[1]))
-	return (-1);
+		return (-1);
+
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
-	return (-1);
+		return (-1);
+
 	va_start(args, format);
-	while (*p != '\0')
-{
-	if (*p == '%')
-{
-		p++;
-	switch (*p)
-{
-	case 'c':
-		count += _putchar(va_arg(args, int));
-	break;
 
-	case 's':
-		count += _puts(va_arg(args, char *));
-	break;
-
-	case '%':
-		count += _putchar('%');
-	break;
-	default:
-		count += _putchar('%');
-		count += _putchar(*p);
-	break;
-}
-}
-	else
+	while (*format != '\0')
 	{
-		count += _putchar(*p);
+		if (*format == '%')
+		{
+		count += _handle_format_specifier(&format, args);
+		}
+		else
+		{
+		count += _putchar(*format);
+		format++;
+		}
+	}
+
+	va_end(args);
+	return (count);
 }
-		p++;
-}
-		va_end(args);
-		return (count);
+
+/**
+ * _handle_format_specifier - handles format specifier and prints corresponding
+ * argument(s)
+ * @format: pointer to format string
+ * @args: argument list
+ *
+ * Return: the number of characters printed
+ */
+int _handle_format_specifier(const char **format, va_list args)
+{
+	int count = 0;
+
+	switch (*++(*format))
+	{
+		case 'c':
+		count += _putchar(va_arg(args, int));
+		break;
+
+		case 's':
+		count += _puts(va_arg(args, char *));
+		break;
+
+		case '%':
+		count += _putchar('%');
+		break;
+
+		case 'd':
+		case 'i':
+		count += _print_number(va_arg(args, int));
+		break;
+
+		default:
+		count += _putchar('%');
+		count += _putchar(*(*format - 1));
+		break;
+	}
+
+	return (count);
 }
--- main.h
+++ main.h
@@ -6,5 +6,7 @@
 int _printf(const char *format, ...);
 int _putchar(char c);
 int _puts(char *str);
+int _print_number(int n);
+int _handle_format_specifier(const char **format, va_list args);
 
 #endif
