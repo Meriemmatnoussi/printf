fb0488b8792cfbb839921b1bdaef31422b9daca9
ElRoccinante <alaechouiye@gmail.com>
2023-04-25 06:46:40 -0700

    second_push

--- _printf.c
+++ _printf.c
@@ -4,12 +4,55 @@
 #include <unistd.h>
 #include <string.h>
 #include "main.h"
-#include <stdio.h>
-#include <stdarg.h>
-#include <stddef.h>
-#include <unistd.h>
-#include <string.h>
-#include "main.h"
+
+/**
+ * _print_helper - prints output to stdout according to a format
+ * @p: character string containing zero or more directives
+ * @args: args to be printed
+ * Return: the number of characters printed
+ */
+int _print_helper(const char *p, va_list args)
+{
+int n, count = 0;
+char buf[12];
+
+while (*p != '\0')
+{
+	if (*p == '%')
+	{
+		p++;
+		switch (*p)
+		{
+		case 'c':
+			count += _putchar(va_arg(args, int));
+		break;
+
+		case 's':
+			count += _puts(va_arg(args, char *));
+		break;
+		case 'd':
+		case 'i':
+			n = va_arg(args, int);
+			sprintf(buf, "%d", n);
+			count += _puts(buf);
+		break;
+		case '%':
+			count += _putchar('%');
+		break;
+		default:
+			count += _putchar('%');
+			count += _putchar(*p);
+		break;
+		}
+	}
+	else
+	{
+		count += _putchar(*p);
+	}
+	p++;
+}
+	return (count);
+}
 
 /**
  * _printf - prints output to stdout according to a format
@@ -19,52 +62,15 @@
  */
 int _printf(const char *format, ...)
 {
-va_list args;
-int count = 0;
-const char *p = format;
+	va_list args;
+	int count = 0;
+
 	if (!format || (format[0] == '%' && !format[1]))
 	return (-1);
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
 	return (-1);
 	va_start(args, format);
-	while (*p != '\0')
-{
-	if (*p == '%')
-{
-		p++;
-	switch (*p)
-{
-	case 'c':
-		count += _putchar(va_arg(args, int));
-	break;
-
-	case 's':
-		count += _puts(va_arg(args, char *));
-	break;
-	case 'd':
-	case 'i':
-	{
-		char buf[12]; /* big enough to hold INT_MIN as a string */
-		int n = va_arg(args, int);
-		sprintf(buf, "%d", n);
-		count += _puts(buf);
-		break;
-	}
-	case '%':
-		count += _putchar('%');
-	break;
-	default:
-		count += _putchar('%');
-		count += _putchar(*p);
-	break;
-}
-}
-	else
-	{
-		count += _putchar(*p);
-}
-		p++;
-}
-		va_end(args);
-		return (count);
+	count = _print_helper(format, args);
+	va_end(args);
+	return (count);
 }
--- main.h
+++ main.h
@@ -6,5 +6,6 @@
 int _printf(const char *format, ...);
 int _putchar(char c);
 int _puts(char *str);
+int _print_helper(const char *p, va_list args);
 
 #endif
