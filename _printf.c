5eeb436068e938d83c4556414fcbd209c42f1ec2
Meriemmatnoussi <meriem.matnoussi123@gmail.com>
2023-04-24 18:53:07 -0700

    i've done nothing

--- _printf.c
+++ _printf.c
@@ -13,70 +13,44 @@
  */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int count = 0;
-
+va_list args;
+int count = 0;
+const char *p = format;
 	if (!format || (format[0] == '%' && !format[1]))
-		return (-1);
-
+	return (-1);
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
-
+	return (-1);
 	va_start(args, format);
-
-	while (*format != '\0')
-	{
-		if (*format == '%')
-		{
-		count += _handle_format_specifier(&format, args);
-		}
-		else
-		{
-		count += _putchar(*format);
-		format++;
-		}
-	}
-
-	va_end(args);
-	return (count);
-}
-
-/**
- * _handle_format_specifier - handles format specifier and prints corresponding
- * argument(s)
- * @format: pointer to format string
- * @args: argument list
- *
- * Return: the number of characters printed
- */
-int _handle_format_specifier(const char **format, va_list args)
+	while (*p != '\0')
 {
-	int count = 0;
-
-	switch (*++(*format))
-	{
-		case 'c':
+	if (*p == '%')
+{
+		p++;
+	switch (*p)
+{
+	case 'c':
 		count += _putchar(va_arg(args, int));
-		break;
+	break;
 
-		case 's':
+	case 's':
 		count += _puts(va_arg(args, char *));
-		break;
+	break;
 
-		case '%':
+	case '%':
 		count += _putchar('%');
-		break;
-
-		case 'd':
-		case 'i':
-		count += _print_number(va_arg(args, int));
-		break;
-
-		default:
+	break;
+	default:
 		count += _putchar('%');
-		count += _putchar(*(*format - 1));
-		break;
-	}
-
-	return (count);
+		count += _putchar(*p);
+	break;
+}
+}
+	else
+	{
+		count += _putchar(*p);
+}
+		p++;
+}
+		va_end(args);
+		return (count);
 }
--- main.h
+++ main.h
@@ -6,7 +6,5 @@
 int _printf(const char *format, ...);
 int _putchar(char c);
 int _puts(char *str);
-int _print_number(int n);
-int _handle_format_specifier(const char **format, va_list args);
 
 #endif
